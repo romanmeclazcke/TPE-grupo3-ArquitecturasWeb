67455e294db2f46e7605090abe130588ab21c5fd
Maia Manze <maia.manze@gmail.com>
2024-09-23 15:58:45 -0300

    agregamos factory y singleton

--- TPE-grupo3-ArquitecturasWeb/Entregable1/src/main/java/main.java
+++ TPE-grupo3-ArquitecturasWeb/Entregable1/src/main/java/main.java
@@ -3,6 +3,7 @@ import DAO.Factura_ProductoDAO;
 import DAO.ProductoDAO;
 import DTO.ClienteDTO;
 import DTO.ProductoDTO;
+import Factory.DAOFactory;
 import Utils.HelperMySQL;
 
 import java.io.IOException;
@@ -13,15 +14,16 @@ public class main {
     public static void main(String[] args) throws IOException, SQLException {
         HelperMySQL hp = new HelperMySQL();
         hp.createTables();
-        //hp.insertAll();
+        hp.insertAll();
 
-        ArrayList<ClienteDTO> aux = new ClienteDAO(hp.getConnection()).getClientes();
+        ArrayList<ClienteDTO> aux = DAOFactory.getInstance(1).getClienteDAO().getClientes();
+
         for (ClienteDTO cliente : aux) {
             System.out.println(cliente);
         }
 
 
-        ProductoDTO producto= new Factura_ProductoDAO(hp.getConnection()).getProductoMayorRecaudacion();
+        ProductoDTO producto= DAOFactory.getInstance(1).getFacturaProductoDAO().getProductoMayorRecaudacion();
         System.out.println(producto);
 
 
