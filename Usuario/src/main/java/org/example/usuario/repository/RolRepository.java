87e172325d71b1081730d281b55744746c720cd4
Roman Meclazcke <137325701+romanmeclazcke@users.noreply.github.com>
2024-10-31 10:41:04 -0300

    Delete Usuario directory

--- Usuario/src/main/java/org/example/usuario/repository/RolRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.usuario.repository;
-
-import org.example.usuario.entity.Rol;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RolRepository extends JpaRepository<Rol, Long> {
-}
--- Usuario/src/main/java/org/example/usuario/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.usuario.repository;
-
-import org.example.usuario.entity.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
