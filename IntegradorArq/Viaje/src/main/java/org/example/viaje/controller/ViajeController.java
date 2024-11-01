bd7ab3c44330bc9ca5f57ad45400daa080ab34d6
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2024-11-01 11:26:20 -0300

    cambios en usuarios

--- IntegradorArq/Viaje/src/main/java/org/example/viaje/controller/ViajeController.java
+++ IntegradorArq/Viaje/src/main/java/org/example/viaje/controller/ViajeController.java
@@ -3,7 +3,6 @@ package org.example.viaje.controller;
 
 import org.example.viaje.service.ViajeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
