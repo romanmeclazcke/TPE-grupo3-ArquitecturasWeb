3f8c4d89d120d85fae3cbd8569d76626feb30c53
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2024-11-03 19:12:09 -0300

    Cree metodos para poder generar reporte (falta implementar la consutla del orden en monopatin)

--- IntegradorArq/Reporte/src/main/java/org/example/reporte/controller/ReporteController.java
+++ IntegradorArq/Reporte/src/main/java/org/example/reporte/controller/ReporteController.java
@@ -1,9 +1,14 @@
 package org.example.reporte.controller;
 
 
+import org.example.reporte.DTO.ReporteDto;
 import org.example.reporte.service.ReporteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,4 +17,13 @@ public class ReporteController {
 
     @Autowired
     ReporteService reporteService;
+
+    @GetMapping("")
+    public ResponseEntity<?> obtenerReportePorTipo(@RequestParam(required = true) String tipo) {
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body(this.reporteService.obtenerReportePorTipo(tipo));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
 }
--- IntegradorArq/Reporte/src/main/java/org/example/reporte/service/ReporteService.java
+++ IntegradorArq/Reporte/src/main/java/org/example/reporte/service/ReporteService.java
@@ -1,13 +1,31 @@
 package org.example.reporte.service;
 
 
+import org.example.reporte.DTO.ReporteDto;
+import org.example.reporte.feignClients.MonopatinFeignClient;
 import org.example.reporte.repository.ReporteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ReporteService {
 
     @Autowired
     ReporteRepository reporteRepository;
+
+    @Autowired
+    MonopatinFeignClient monopatinFeignClient;
+
+
+    public ReporteDto obtenerReportePorTipo(String tipoReporte){
+        ReporteDto reporteDto = new ReporteDto();
+        if(tipoReporte!="kilometros" || tipoReporte!="conPausa"||tipoReporte!="sinPausa"){
+             reporteDto.setExito(false);
+             reporteDto.setMensaje("Solo podes filtar por kilometors, conPausa, SinPausa");
+        }
+        reporteDto.setMontopatines(this.monopatinFeignClient.getMonopatinesOrdenadosPor(tipoReporte));
+        return reporteDto;
+    }
 }
