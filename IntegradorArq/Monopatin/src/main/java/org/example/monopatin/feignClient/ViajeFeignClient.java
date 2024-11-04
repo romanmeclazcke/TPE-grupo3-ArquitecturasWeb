211bdba831e23ec4fc488b4753d822ca31e8e947
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2024-11-04 15:16:21 -0300

    Solucione error al crear el feign client en monopatin

--- IntegradorArq/Monopatin/src/main/java/org/example/monopatin/feignClient/ViajeFeignClient.java
+++ IntegradorArq/Monopatin/src/main/java/org/example/monopatin/feignClient/ViajeFeignClient.java
@@ -1,7 +1,7 @@
 package org.example.monopatin.feignClient;
 
 
-import org.example.viaje.DTO.*;
+//import org.example.viaje.DTO.*;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 public interface ViajeFeignClient {
 
     @PostMapping("monopatinId/{monopatinId}/usuarioId/{usuarioId}")
-    ViajeResponseDTO createViaje(@PathVariable Long monopatinId, Long usuarioId);
+    void createViaje(@PathVariable Long monopatinId, Long usuarioId);
 
 }
