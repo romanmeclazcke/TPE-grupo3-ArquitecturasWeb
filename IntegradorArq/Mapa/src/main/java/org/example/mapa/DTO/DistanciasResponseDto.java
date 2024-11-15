c01404200960631faaad04371a2d76432083d559
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2024-11-15 13:44:12 -0300

    agregue documentacion para microservicio de mapa

--- IntegradorArq/Mapa/src/main/java/org/example/mapa/DTO/DistanciasResponseDto.java
+++ IntegradorArq/Mapa/src/main/java/org/example/mapa/DTO/DistanciasResponseDto.java
@@ -1,6 +1,7 @@
 package org.example.mapa.DTO;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class DistanciasResponseDto {
+
+    @Schema(description = "Identificador único de la parada de origen", example = "1")
     private Long idParadaOrigen;
+
+    @Schema(description = "Identificador único de la parada de destino", example = "2")
     private Long idParadaDestino;
+
+    @Schema(description = "Distancia en kilómetros entre las paradas", example = "15.5")
     private Double distancia;
 }
