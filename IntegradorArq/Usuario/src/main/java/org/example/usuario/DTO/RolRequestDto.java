12575074bbec8551f65030c212a242c3abb3cfce
Martin <acostamartin1270@gmail.com>
2024-11-19 18:24:27 -0300

    Documentado Usuario completo + dtos

--- IntegradorArq/Usuario/src/main/java/org/example/usuario/DTO/RolRequestDto.java
+++ IntegradorArq/Usuario/src/main/java/org/example/usuario/DTO/RolRequestDto.java
@@ -1,5 +1,6 @@
 package org.example.usuario.DTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -12,8 +13,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RolRequestDto {
 
+    @Schema(description = "Id autogenerado del rol", example = "1")
     private Long id; // Número identificatorio
 
+    @Schema(description = "Nombre del rol", example = "ADMINISTRADOR")
     @NotEmpty(message = "el campo nombre del rol no puede estar vacío")
     @NotNull( message = "El nombre deel rol es un campo obligatorio.")
     private String tipo_rol;
--- IntegradorArq/Usuario/src/main/java/org/example/usuario/DTO/UsuarioResponseDto.java
+++ IntegradorArq/Usuario/src/main/java/org/example/usuario/DTO/UsuarioResponseDto.java
@@ -1,5 +1,6 @@
 package org.example.usuario.DTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,12 +11,19 @@ import org.example.usuario.entity.Rol;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UsuarioResponseDto {
+    @Schema(description = "Id autogenerado de usuario" , example = "1")
     private Long id;
+    @Schema(description = "Nombre del usuario", example = "Charles")
     private String nombre;
+    @Schema(description = "Apellido del usuario", example = "Darwin")
     private String apellido;
+    @Schema(description = "Numero de telefono asociado al usuario", example ="2494332088")
     private Long numeroCelular;
+    @Schema(description = "Email del usuario", example = "charlesdarwin@gmail.com")
     private String email;
+    @Schema(description = "Contraseña asignada al usuario", example = "asd123")
     private String password;
+    @Schema(description = "Rol asignado al usuario" , example = "2")
     private Rol rol;
 
     private String mensaje;
